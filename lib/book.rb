c5814d4e88f2f0606f251b359c9286726ec25379
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 17:55:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -45,5 +45,12 @@ class Book
   def initialize(title)
     @title = title
   end
-  
+
+  def title
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    
 end
