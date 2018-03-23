cb8dbc85b7effb8e4e3f3876b015eb38218b9b9a
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 17:53:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,4 @@
+=begin
 class Book
   def initialize(title)
     @title = title
@@ -35,4 +36,14 @@ def turn_page
   puts "Flipping the page...wow, you read fast!"
 end
 
+end
+=end
+
+class Book
+  attr_accessor :author, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+  
 end
